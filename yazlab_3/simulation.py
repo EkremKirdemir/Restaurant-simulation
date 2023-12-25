f3c376174a876702a2a6fd5af467c8acbdbb2ce4
EkremKirdemir <kirdemir.ekrem@gmail.com>
2023-12-25 16:29:35 +0300

    final

--- yazlab_3/simulation.py
+++ yazlab_3/simulation.py
@@ -20,6 +20,9 @@ waiter_count = 3
 chef_count = 2
 cash_register_count = 1
 
+customer_table_assignments = {}
+customer_status = {}
+
 waiter_semaphore = threading.Semaphore(waiter_count)
 chef_semaphore = threading.Semaphore(chef_count)
 table_semaphore = threading.Semaphore(table_count)
@@ -47,12 +50,17 @@ def assign_table(customer_id):
         for table_id, assigned_customer in table_assignments.items():
             if assigned_customer is None:
                 table_assignments[table_id] = customer_id
+                logMessage(f"Table {table_id} assigned to Customer {customer_id}.")
                 return table_id
-        return None
+    logMessage(f"No available table for Customer {customer_id}.")
+    return None
+
+
 
 def release_table(table_id):
     with table_lock:
         table_assignments[table_id] = None
+        logMessage(f"Table {table_id} released and now available.")
 def update_queue_status(action, customer_id,status):
     async_to_sync(channel_layer.group_send)(
         'table_group',
@@ -111,14 +119,16 @@ def update_table_status(table_id, status):
         }
     )
 
-def manage_tables():
-    while True:
-        priority, arrival_time, customer_id, customer_event = table_queue.get()
-        table_semaphore.acquire() 
-        status = "Priority customer" if priority else "Customer"
-        logMessage(f"{status} {customer_id} has been seated at a table.")
-        customer_event.set() 
-        table_queue.task_done()
+
+
+def release_table(table_id):
+    with table_lock:
+        table_assignments[table_id] = None
+        logMessage(f"Table {table_id} released and now available.")
+        table_semaphore.release()
+
+
+
 
 def chef(chef_id):
     while True:
@@ -181,52 +191,95 @@ def manage_cash_registers():
         update_cashRegister_status(1,"Available")
         customer_event.set() 
         cash_register_queue.task_done()
+def manage_tables():
+    while True:
+        priority, arrival_time, customer_id, customer_event = table_queue.get()
 
 
-def customer_process(customer_id,priority):
+        if table_semaphore.acquire(blocking=True, timeout=20):
+    
+            if customer_status.get(customer_id) == "left":
+                logMessage(f"Customer {customer_id} already left, cannot be seated.")
+                table_semaphore.release()
+                table_queue.task_done()
+                continue
+            table_id = assign_table(customer_id)
+            if table_id is not None:
+                customer_table_assignments[customer_id] = table_id
+                logMessage(f"Customer {customer_id} seated at table {table_id}.")
+                update_table_status(table_id, f"Customer {customer_id} seated.")
+                customer_event.set()
+            else:
+        
+                table_semaphore.release()
+                logMessage(f"Error: Semaphore acquired but no table was actually free for Customer {customer_id}.")
+        else:
+    
+            logMessage(f"Customer {customer_id} left the restaurant after waiting too long for a table.")
+            customer_table_assignments.pop(customer_id, None)
+
+        table_queue.task_done()
+
+def customer_process(customer_id, priority):
     customer_event = threading.Event()
     eating_event = threading.Event()
     payment_event = threading.Event()
 
     status = "Priority customer" if priority else "Customer"
-    update_queue_status('add_to_queue', customer_id,status)
+    update_queue_status('add_to_queue', customer_id, status)
+    customer_status[customer_id] = "waiting"
 
-    if not table_semaphore.acquire(timeout=20):
-        update_queue_status('remove_from_queue', customer_id,status)
-        update_queue_status('add_to_left', customer_id,status)
+    customer_priority = 0 if priority else 1
+    table_queue.put((customer_priority, time.time(), customer_id, customer_event))
+
+    seated = customer_event.wait(timeout=20)
+    if not seated:
         logMessage(f"{status} {customer_id} left the restaurant after waiting too long for a table.")
-        return 
-
-
-    table_id = assign_table(customer_id)
-    if table_id is None:
-        logging.error(f"Error in table assignment for {status} {customer_id}")
-        table_semaphore.release()
+        customer_table_assignments.pop(customer_id, None)
+        customer_status[customer_id] = "left"
+        update_queue_status('remove_from_queue', customer_id, status)
+        update_queue_status('add_to_left', customer_id, status)
         return
-    update_queue_status('remove_from_queue', customer_id,status)
+
+    table_id = customer_table_assignments.get(customer_id)
+    if table_id is None:
+        logMessage(f"Error in table assignment for {status} {customer_id}")
+        return
+
+    update_queue_status('remove_from_queue', customer_id, status)
     logMessage(f"{status} {customer_id} has been seated at table {table_id}.")
     update_table_status(table_id, f"{status} {customer_id} is waiting for waiter")
-    logMessage(f"{status} {customer_id} is waiting for waiter at table {table_id}")
+    customer_status[customer_id] = "seated"
 
-    waiter_queue.put((priority, customer_id, customer_id, eating_event))
-    eating_event.wait() 
-    eating_event.clear()
+    waiter_queue.put((priority, time.time(), customer_id, eating_event))
+    eating_event.wait()
 
+    if customer_status[customer_id] == "left":
+        return
 
-
-    update_table_status(table_id, f"{status} {customer_id} is eating")
     logMessage(f"{status} {customer_id} is eating")
-    time.sleep(3) 
-    logMessage(f"{status} {customer_id} finished eating at table {table_id}.")
-    update_table_status(table_id, "Available")
+    update_table_status(table_id, f"{status} {customer_id} is eating")
+    customer_status[customer_id] = "eating"
+    time.sleep(3)
+
+    cash_register_queue.put((priority, time.time(), customer_id, payment_event))
+    payment_event.wait()
+
+    if customer_status[customer_id] == "left":
+        return
+
+    logMessage(f"{status} {customer_id} is paying.")
+    update_cashRegister_status(1, f"{status} {customer_id} is paying.")
+    customer_status[customer_id] = "paying"
+    time.sleep(1)
 
     release_table(table_id)
-    table_semaphore.release()
+    logMessage(f"Table {table_id} released by {status} {customer_id}.")
+    update_table_status(table_id, "Available")
+    customer_status[customer_id] = "completed"
+    customer_table_assignments.pop(customer_id, None)
 
 
-    cash_register_queue.put((priority, customer_id, customer_id, payment_event))
-    payment_event.wait() 
-    payment_event.clear()
 
 
 def start_simulation(customer_list,customer_interval):
